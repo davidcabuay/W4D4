7aaeb5927b46b5f3f621750c771d62a3ef8ffdbf
David <david.cabuay@gmail.com>
2022-12-22 14:51:55 -0800

    Finished bonus anagram

--- anagrams.rb
+++ anagrams.rb
@@ -8,6 +8,7 @@ letters2= Hash.new(0)
     letters1==letters2
 end
 
+#O(n^3)
 def second_anagram?(string1, string2)
     str2arr = string2.split("")
     string1.each_char.with_index do |char, i|
@@ -19,9 +20,36 @@ def second_anagram?(string1, string2)
     str2arr.empty?
 end
 
+#O(n*logn) n + n + nlogn + nlogn => nlogn
+def third_anagram?(string1, string2)
+    first = string1.split("")
+    second = string1.split("")
+    f = first.sort 
+    s= second.sort
+    f == s
+    # string1.split("").sort == string2.split("").sort  
+end
+
+# O(n) n + n => n
+def fourth_anagram?(string1, string2)
+    letters1= Hash.new(0)
+    letters2= Hash.new(0)
+        string1.each_char {|char|letters1[char]+=1}
+        string2.each_char {|char| letters2[char]+=1}
+        letters1==letters2
+end
+
+#O(n) n + n + n + n => n
+def fifth_anagram?(string1, string2)
+    letters = Hash.new(0)
+
+    string1.each_char {|char| letters[char] += 1}
+    string2.each_char {|char| letters[char] -= 1}
+    value = letters.values
+    value.all?{ |num| num == 0}
+    
+end
 
 
-
-
-p second_anagram?("gizmo", "sally")    #=> false
-p second_anagram?("elvis", "llives")    #=> true
+p fifth_anagram?("gizmo", "sally")    #=> false
+p fifth_anagram?("elllvis", "lllives")    #=> true
